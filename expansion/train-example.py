55276ebd241cedcdc6fff37856ffb5d1e08f4af5
DrJupiter <klausjupiter@hotmail.com>
2023-09-11 12:15:31 +0200

    ex

--- expansion/train-example.py
+++ expansion/train-example.py
@@ -67,8 +67,8 @@ model = UNet2DModel(
     sample_size=config.image_size,  # the target image resolution
     in_channels=3,  # the number of input channels, 3 for RGB images
     out_channels=3,  # the number of output channels
-    layers_per_block=2,  # how many ResNet layers to use per UNet block
-    block_out_channels=(128, 128, 256, 256, 512, 512),  # the number of output channels for each UNet block
+    layers_per_block=3,  # how many ResNet layers to use per UNet block
+    block_out_channels=(128, 256, 256, 512, 1024, 1024),  # the number of output channels for each UNet block
     down_block_types=(
         "DownBlock2D",  # a regular ResNet downsampling block
         "DownBlock2D",
