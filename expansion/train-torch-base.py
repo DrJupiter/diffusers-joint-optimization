9217bdb328e68b5a0cb53b9dabc316ce53fbc2df
DrJupiter <klausjupiter@hotmail.com>
2023-10-23 17:45:59 +0200

    torch: smaller net higher es

--- expansion/train-torch-base.py
+++ expansion/train-torch-base.py
@@ -80,10 +80,32 @@ def main():
     text_encoder.requires_grad_(False)
 
     # TODO Make this a class containing the SDE and the UNET
+    #unet = UNet2DConditionModel(sample_size=config.training.resolution,
+    #                            in_channels=3,
+    #                            out_channels=3,
+    #                            cross_attention_dim=768 # TODO (KLAUS) : EXTRACT THIS NUMBER FROM CLIP MODEL
+    #                            )
     unet = UNet2DConditionModel(sample_size=config.training.resolution,
                                 in_channels=3,
                                 out_channels=3,
-                                cross_attention_dim=768 # TODO (KLAUS) : EXTRACT THIS NUMBER FROM CLIP MODEL
+                                block_out_channels=(128, 128, 256, 256, 512, 512),  # the number of output channels for each UNet block
+                                down_block_types=(
+                                    "DownBlock2D",  # a regular ResNet downsampling block
+                                    "DownBlock2D",
+                                    "DownBlock2D",
+                                    "DownBlock2D",
+                                    "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+                                    "DownBlock2D",
+                                ),
+                                up_block_types=(
+                                    "UpBlock2D",  # a regular ResNet upsampling block
+                                    "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+                                    "UpBlock2D",
+                                    "UpBlock2D",
+                                    "UpBlock2D",
+                                    "UpBlock2D",
+                                ),
+                                cross_attention_dim=768, # TODO (KLAUS) : EXTRACT THIS NUMBER FROM CLIP MODEL
                                 )
     unet.train()    
 
