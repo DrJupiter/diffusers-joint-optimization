8456aa7012b4b9649f3642a6c194fa0ec426e255
DrJupiter <klausjupiter@hotmail.com>
2023-10-26 01:52:57 +0200

    toast: sde sampling works

--- expansion/sde_redefined.py
+++ expansion/sde_redefined.py
@@ -246,6 +246,7 @@ class SDE:
 
             dxdt = Fx + diffusion_term 
             print(f"{Fx=}, {L=}, {dxdt=}") 
+            print(self.diffusion.diffusion)
             # Our SDE: dx(t) = [ F(t)x(t) - L(t)L(t)^T score ] dt + L(t) db(t)
 
             # Yang Song dx = [ f(x,t)-g^2 score ] dt + g(t) dw
